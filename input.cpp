4ed30337140a0fbb9f074e24c8a347507fc4285f
Ayoub Taouarda <ayoubtrdone@gmail.com>
2020-12-23 19:39:10 +0100

    initial commit

---
+++ input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+
+    int slices;
+    std::cout << "Enter the number of slices of pizza you eat: ";
+    std::cin >> slices;
+
+    std::cout << "You eat " << slices << " slices of pizza\n";
+
+    return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int slices = 10;
+    
+    std::cout << "You have " << slices << " slices of pizza." << "\n";
+
+    return 0;
+}
